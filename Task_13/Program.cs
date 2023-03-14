8b912961ef2cf2f37935079197f75b5645c11943
ViboKozus <126609888+ViboKozus@users.noreply.github.com>
2023-03-14 20:48:59 +0300

    Update Program.cs

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,21 +1,33 @@
-﻿#pragma warning disable
+#pragma warning disable
 
 Console.Clear ();
 Console.WriteLine ("Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
 
 int N; 
 Console.WriteLine($"введите число N"); 
-int.TryParse(Console.ReadLine()!, out N); 
+int.TryParse(Console.ReadLine ()!, out N); 
  
 
-int modN = N; // модуль N если оно меньше 1, то просто умножим его на -1 
+int absN = N; 
 if (N < 0) 
-    modN = -1 * N; 
-  
-if (modN > 99) // в положительном числе есть 3-я цифра, если оно больше 99 
+    absN = -1 * N; 
+ 
+int result = absN; 
+ 
+
+if (result > 99)  
 { 
-    int result = (modN / 100) % 10; 
+
+    while (result > 999)  
+    { 
+        result = result / 10; 
+    } 
+    
+    result = result % 10; 
+ 
     Console.WriteLine($"3-я цифра в {N} -- это {result}"); 
 } 
-else 
-    Console.WriteLine($"в числе {N} меньше 3-х циф");
+else  
+{ 
+    Console.WriteLine($"в числе {N} меньше 3-х циф"); 
+}
